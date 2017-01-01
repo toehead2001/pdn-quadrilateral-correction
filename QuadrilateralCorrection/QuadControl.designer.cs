7c8c1457c6f0da1e663f76261fe4f80ec183274e
toehead2001 <toe_head2001@yahoo.com>
2017-01-01 01:53:04 -0800

    Just stretch the image to avoid transparent edges
    The aspect ratio should 99.99% correct, so no distortion should happen.

--- QuadrilateralCorrection/QuadControl.designer.cs
+++ QuadrilateralCorrection/QuadControl.designer.cs
@@ -40,6 +40,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(340, 280);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
