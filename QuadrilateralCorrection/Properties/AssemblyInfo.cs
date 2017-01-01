94608766a89cf7e0d9e6cda36e5b59d42481230e
toehead2001 <toe_head2001@yahoo.com>
2017-01-01 02:54:18 -0800

    Update Assembly Info and Bump version to 1.1

--- QuadrilateralCorrection/Properties/AssemblyInfo.cs
+++ QuadrilateralCorrection/Properties/AssemblyInfo.cs
@@ -2,12 +2,12 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Quadrilateral Correction plugin for Paint.NET")]
-[assembly: AssemblyDescription("Quadrilateral Correction")]
+[assembly: AssemblyDescription("Remaps a defined quadrilateral to a rectangular image.")]
 [assembly: AssemblyConfiguration("quadrilateral correction")]
 [assembly: AssemblyCompany("toe_head2001")]
 [assembly: AssemblyProduct("Quadrilateral Correction")]
-[assembly: AssemblyCopyright("Copyright © toe_head2001")]
+[assembly: AssemblyCopyright("Copyright © 2017 toe_head2001")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
